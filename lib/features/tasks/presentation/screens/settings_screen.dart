f511a2559b8ffd5b2db55f80cab4f87ee1357a64
Melbin Babu <melbinbabu66@gmail.com>
2026-03-12 01:15:40 +0530

    settings screen widget refactoring

--- lib/features/tasks/presentation/screens/settings_screen.dart
+++ lib/features/tasks/presentation/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/widgets/app_button.dart';
+import '../../../../core/widgets/app_confirmation_dialog.dart';
 import '../../../../core/widgets/app_text.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -22,37 +23,64 @@ class SettingsScreen extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(20.w),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24.r),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AppText.title('Account'),
-                        SizedBox(height: 8.h),
-                        const AppText.medium('Manage your session from here.'),
-                        SizedBox(height: 20.h),
-                        AppButton(
-                          label: 'Logout',
-                          icon: const Icon(Icons.logout_rounded),
-                          variant: AppButtonVariant.destructive,
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            onLogout();
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
+                  UserSessionCard(onLogout: onLogout),
                 ],
               ),
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class UserSessionCard extends StatelessWidget {
+  const UserSessionCard({
+    super.key,
+    required this.onLogout,
+  });
+
+  final VoidCallback onLogout;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(20.w),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24.r),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText.title('Account'),
+          SizedBox(height: 8.h),
+          const AppText.medium('Manage your session from here.'),
+          SizedBox(height: 20.h),
+          AppButton(
+            label: 'Logout',
+            icon: const Icon(Icons.logout_rounded),
+            variant: AppButtonVariant.destructive,
+            onPressed: () async {
+              final shouldLogout = await showAppConfirmationDialog(
+                context,
+                title: 'Logout?',
+                message:
+                    'You will be signed out of your account on this device.',
+                confirmLabel: 'Logout',
+                isDestructive: true,
+              );
+    
+              if (!shouldLogout || !context.mounted) {
+                return;
+              }
+    
+              Navigator.of(context).pop();
+              onLogout();
+            },
+          ),
+        ],
       ),
     );
   }
